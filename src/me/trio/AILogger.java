e998446b30f1c68bb8cc33371c5d65e1647f0cfa
Someoneawesome78 <Someoneawesome78@github.com>
2019-10-11 20:03:46 -0400

    Added Loggers

---
+++ src/me/trio/AILogger.java
@@ -0,0 +1,100 @@
+package me.trio;
+
+import java.io.File;
+
+import org.newdawn.slick.Graphics;
+
+public class AILogger implements GameObject {
+	
+	private static AILogger log;
+	
+	/**
+	 * Gets the current logger
+	 */
+	public static AILogger getLogger() {
+		return log;
+	}
+	
+	private DebugLogging logFile;
+	private File dataFile;
+
+	@Override
+	public void start() {
+
+		log = this;
+		
+		String dataFolder = System.getenv("LOCALAPPDATA");
+		
+		logFile = new DebugLogging(dataFolder + "\\AIStuff");
+		
+	}
+
+	@Override
+	public void earlyUpdate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void lateUpdate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setEnable(boolean enable) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stop() {
+		logFile.close();
+	}
+	
+	/**
+	 * Logs a message 
+	 * @param m the message to log
+	 */
+	public void log(String m) {
+		System.out.println(m);
+		logFile.log(m);
+	}
+	
+	/**
+	 * Logs a message as a warning
+	 * @param m the message to log
+	 */
+	public void warn(String m) {
+		System.out.print("[WARNING] " + m);
+		logFile.log("[WARNING] " + m);
+	}
+	
+	/**
+	 * Logs a message as a Error
+	 * @param m the message to log
+	 */
+	public void err(String m) {
+		System.err.println(m);
+		logFile.log("[ERROR] " + m);
+	}
+
+	@Override
+	public void onRender(Graphics graphic) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
